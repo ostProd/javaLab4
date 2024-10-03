148f57c82e1cfbb102d27662a50bf60a57c099ae
Victoria <kryvshenko.viktoriya@lll.kpi.ua>
2024-10-03 19:17:43 +0300

    Додала 38 завдання

---
+++ src/num38.java
@@ -0,0 +1,33 @@
+public class num38 {
+    public static void main(String[] args){
+    int [] arr1 = {3, 1, 17, -4, 12};
+    int [] arr2 = {5, -12, 5, -9, 15};
+    int [] arr3 = {1, 2};
+    try {
+        elements(arr1);
+        elements(arr2);
+        elements(arr3);
+    }
+    catch(Exception e){
+        System.out.println(e);
+    }
+
+    }
+    public static void elements(int [] arr) {
+        if (arr.length < 3) {
+            throw new ArrayIndexOutOfBoundsException("Масив має складатись хоча б з 3 елементів");
+        }
+        int thirdEl = arr[2];
+        int maxEl = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxEl) {
+                maxEl = arr[i];
+            }
+        }
+        int sum = maxEl + thirdEl;
+        System.out.println(sum);
+    }
+
+}
+
+
