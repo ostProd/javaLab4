2b36e2dfb436d148cb415d1d71157d88d6d11d40
SodolinskiyMisha <mishasodolinskiy@gmail.com>
2024-10-03 19:38:12 +0300

    Сума елементів,що кратні 3

---
+++ src/SumOfMultiplesOfThree.java
@@ -0,0 +1,14 @@
+public class SumOfMultiplesOfThree {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int sum = 0;
+
+        for (int number : numbers) {
+            if (number % 3 == 0) {
+                sum += number;
+            }
+        }
+
+        System.out.println("Сума елементів, кратних 3: " + sum);
+    }
+}
